1ddc685a671828e29a7afa7ae431b1728a751914
Zach Fisher <zfisher2753@gmail.com>
2023-04-03 10:16:58 -0400

    basic searchSingle and searchPhrase methods

---
+++ Search.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Search {
+    public ArrayList<Url> searchSingle(String queryIn) {
+        UrlDao uDao = new UrlDao();
+        ArrayList<Object> allURLs = uDao.selectAll();
+        ArrayList<Url> results = new ArrayList<Url>();
+        
+        // Find the starting position of the query in each the CLOB of each URL and add to results if found
+        for (Object entry: allURLs) {
+            Url foundUrl = (Url) entry;
+            try {
+                if (foundUrl.text.position(queryIn, 0) > -1) {
+                    results.add(foundUrl);
+                }
+            }
+            catch (Exception e) {
+                continue;
+            }
+        }
+
+        return results;
+    }
+
+    public ArrayList<Url> searchPhrase(String queryIn) {
+        // Search each word in the phrase individually
+        String[] words = queryIn.split(" ");
+        ArrayList<ArrayList<Url>> queryResults = new ArrayList<ArrayList<Url>>();
+        for (String entry: words) {
+            queryResults.add(searchSingle(entry));
+        }
+
+        // Automatically add everything containing the full string to results
+        ArrayList<Url> results = new ArrayList<Url>();
+        for (Url result: searchSingle(queryIn)) {
+            results.add(result);
+        }
+
+        // Add all urls that were found for all individual words to results
+        for (Url foundUrl: queryResults.get(0)) {
+            int timesFound = 0;
+            for (int i = 1; i < queryResults.size(); ++i) {
+                if (queryResults.get(i).indexOf(foundUrl) != -1) {
+                    timesFound += 1;
+                }
+            }
+            if (timesFound == queryResults.size() - 1) {
+                results.add(foundUrl);
+            }
+        }
+
+
+
+        return results;
+    }
+}
